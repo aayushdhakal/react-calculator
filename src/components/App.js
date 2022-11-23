c68ae078728cd48cbd115ec5ca7d74f9ffb89587
aayush dhakal <aayushdhakal96@gmail.com>
2022-11-23 15:04:33 +0545

    adding the functionality of removing the history list as a whole and single as well

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 import Model from 'react-modal';
 import { Link, NavLink } from 'react-router-dom';
 
@@ -10,6 +11,9 @@ class App extends React.Component {
       super(props)
       this.calculateNumber = this.calculateNumber.bind(this);
       this.storeHistory = this.storeHistory.bind(this);
+      this.pushExp = this.pushExp.bind(this);
+      this.clearHistory = this.clearHistory.bind(this);
+      this.removeItem = this.removeItem.bind(this);
       this.state = {
          result: 0,
          historyResult: []
@@ -51,28 +55,61 @@ class App extends React.Component {
 
 
       if (!!inputValue.value) {
-         let value = `${inputValue.value} = ${result.textContent}`;
+         // let value = `${inputValue.value} = ${result.textContent}`;
+         let value = {id:uuidv4(), exp: inputValue.value, result: result.textContent }
+
          this.setState({
-            history:tempArr.push(value)
+            history: tempArr.push(value)
          })
 
-         inputValue.value ='';
+         // console.log(this.state.historyResult);
+         inputValue.value = '';
          result.textContent = 0
       }
+   }
+
+   pushExp(value) {
+      const inputField = document.querySelector('.inputField')
+      const resultField = document.querySelector('.result')
+
+      inputField.value = value.exp;
+      resultField.textContent = value.result;
+   }
+
+   clearHistory() {
+      // console.log(1);
+      this.setState({ historyResult: [] });
+   }
+
+   async removeItem(value){
+
+      const newHistoryList = this.state.historyResult.filter(element=>{
+         return element.id !== value;
+      });
+
+      await this.setState({historyResult:newHistoryList})
+      
+      console.log(this.state.historyResult)
    }
 
    render() {
       return (
          <div className="app">
-            <ul className="calcHistory">
-            {this.state.historyResult.map((item,index)=>(
-               <HistoryList item={item} key={index}/>
-            ))}
-            </ul>
+            <div className="calcHistory">
+               <ul className="calcHistoryLists">
+                  {this.state.historyResult.map((item, index) => (
+                     <HistoryList item={item} key={index} exp={this.pushExp} removeItem={this.removeItem}/>
+                  ))}
+                  {this.state.historyResult.length > 0 &&
+                  <button onClick={this.clearHistory} className="btnHistoryClear">&#10006;</button>
+                  }
+
+               </ul>
+            </div>
             <div className="calculator">
                <h2>Simple Calculator</h2>
                <div className="inputFieldNumber">
-                  <input type="text" className="inputField" placeholder="0" onKeyUp={this.calculateNumber} />
+                  <input type="text" className="inputField" placeholder="0" onChange={this.calculateNumber} />
                   <button onClick={this.clearText}>x</button>
                </div>
                <div className="result">{this.state.result}</div>
--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-const  HistoryList = (props)=>(
-   <li>
-      {props.item}
-   </li>
-);
+class HistoryList extends React.Component {
+   constructor(props) {
+      super(props)
+      this.pushExp = this.pushExp.bind(this);
+      this.removeItem = this.removeItem.bind(this);
+   }
+
+   pushExp() {
+      this.props.exp({exp:this.props.item.exp,result:this.props.item.result});
+   }
+   removeItem(e){
+      this.props.removeItem(e.target.parentNode.getAttribute('id'));
+      // this.props.removeItem('value');
+   }
+
+   render() {
+      return (
+         <li className="listItem" id={this.props.item.id}>
+            <p>{this.props.item.exp}={this.props.item.result}</p>
+            <button className="removeListItem" onClick={this.removeItem}>&#128465;</button>
+            <button className="pushOnInput" onClick={this.pushExp}>&#xbb;</button>
+         </li>
+      )
+   }
+}
 
 export default HistoryList;
